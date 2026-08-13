8ad83f604aebef971b557bd7233bbdea5e5172c9
Namit865 <senjaliyanamit123@gmail.com>
2026-08-13 11:15:21 +0530

    Add app_finder.py: build_app_index and find_app_path with prefix ranking

---
+++ actions/app_finder.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path
+
+
+def build_app_index():
+
+    APPDATA_PATH = Path(os.environ["APPDATA"]).joinpath(
+        "Microsoft/Windows/Start Menu/Programs"
+    )
+    PROGRAMDATA_PATH = Path(os.environ["PROGRAMDATA"]).joinpath(
+        "Microsoft/Windows/Start Menu/Programs"
+    )
+
+    index = {}
+    already_exists = 0
+
+    for folder in [APPDATA_PATH, PROGRAMDATA_PATH]:
+        if folder.exists():
+            for file in folder.rglob("*.lnk"):
+                if file.stem.lower() not in index:
+                    index[file.stem.lower()] = str(file)
+                else:
+                    already_exists += 1
+    return index
+
+
+def find_app_path(app_name, index):
+    if app_name in index:
+        return index[app_name]
+    else:
+        index = build_app_index()
+
+    matches = []
+    prefix_matches = []
+
+    for key in index:
+        if app_name in key:
+            if key.startswith(app_name):
+                prefix_matches.append(key)
+            else:
+                matches.append(key)
+
+    pool = prefix_matches if prefix_matches else matches
+
+    if not pool:
+        return None
+    else:
+        best_key = min(pool, key=len)
+        return index[best_key]
